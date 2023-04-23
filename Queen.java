683bae15df96b0224078b0a780d133b0b22ac464
남경식 <redruka12@gmail.com>
2023-04-23 15:09:27 +0900

    퀸 짰음

--- Queen.java
+++ Queen.java
@@ -12,7 +12,100 @@ public class Queen extends ChessPiece{
     @Override
     public String canMove(int fromX, int fromY, int toX, int toY) {
         String str="";
+
+
         // fromX, fromY, toX, toY를 이용하여 움직일 수 있는지 판단
+        if(fromX==toX && fromY==toY){//자기 자신의 위치를 입력했을 때
+            str = "wrong"
+        }
+        else if(fromX==AtoX&&fromY!=toY) {//세로로 이동
+            if (fromY < toY) {//위로 이동
+                for (int i = fromY + 1; i < toY; i++) {//이동 경로에 장애물이 있는지 확인
+                    if (ChessBoard.board[fromX][i] != null) {
+                        str = "wrong";
+                        break;
+                    }
+                }
+            } else {//아래로 이동
+                for (int i = fromY - 1; i > toY; i--) {//이동 경로에 장애물이 있는지 확인
+                    if (ChessBoard.board[fromX][i] != null) {
+                        str = "wrong";
+                        break;
+                    }
+                }
+            }
+        }
+        else if(fromX!=toX&&fromY==toY) {//가로로 이동
+            if(fromX<toX) {//오른쪽으로 이동
+                for(int i=fromX+1;i<toX;i++) {//이동 경로에 장애물이 있는지 확인
+                    if(ChessBoard.board[i][fromY]!=null) {//이동 경로에 장애물이 있으면 이동 불가
+                        str="wrong";
+                        break;
+                    }
+                }
+            }
+            else {
+                for(int i=fromX-1;i>toX;i--) {
+                    if(ChessBoard.board[i][fromY]!=null) {
+                        str="wrong";
+                        break;
+                    }
+                }
+            }
+        }
+
+        else if(fromX!=toX&&fromY!=toY) {//대각선 이동
+            if (fromX - toX == fromY - toY) {//오른쪽 대각선 상승 이동
+                if (fromX < toX && fromY < toY) {
+                    for (int i = 1; i < toX - fromX; i++) {
+                        if (ChassBoard.board[fromX + i][fromY + i]) {
+                            str == "wrong";
+                            break;
+                        }
+                    }
+                } else if (fromX > toX && fromY > toY) {
+                    for (int i = 1; i < fromX - toX; i++) {
+                        if (ChassBoard.board[fromX - i][fromY - i]) {
+                            str == "wrong";
+                            break;
+                        }
+                    }
+                }
+
+            }
+            else if(fromX - toX == -(fromY - toY)) {//오른쪽 대각선 하락 이동
+                if(fromX < toX) {
+                    for (int i = 1; i < toX - fromX; i++) {
+                        if (ChassBoard.board[fromX + i][fromY - i]) {
+                            str == "wrong";
+                            break;
+                        }
+                    }
+                }
+                else{
+                    for(int i = 1; i < fromX - toX; i++) {
+                        if (ChassBoard.board[fromX - i][fromY + i]) {
+                            str == "wrong";
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if(str!="wrong") {
+            if(ChessBoard.board[toX][toY]==null) {//이동 경로에 장애물이 없으면 이동
+                str="move";
+            }
+            else if(ChessBoard.board[toX][toY].isWhite!=ChessBoard.board[fromX][fromY].isWhite) {//이동 경로에 장애물이 있고 적인 기물이 있으면 먹기
+                str="eat";
+            }
+            else {//이동 경로에 장애물이 있고 아군 기물이 있으면 이동 불가
+                str="wrong";
+            }
+        }
+        return str;//이동 불가
+    }
         // 움직일 수 있는 경우, 이동하려는 위치에 기물이 없는 경우, "move" 리턴
         // 움직일 수 있는 경우, 이동하려는 위치에 기물이 없는 경우, 이동하려는 위치에 기물이 있는 경우, 적인 경우, "eat" 리턴
         // 움직일 수 있는 경우, 이동하려는 위치에 기물이 없는 경우, 이동하려는 위치에 기물이 있는 경우, 아군인 경우, "wrong" 리턴
